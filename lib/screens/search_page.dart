46d101dbacf655971d715cd345ec0e7607a486b5
cever10 <jiwan_01@naver.com>
2022-12-01 16:50:48 +0900

    검색페이지 수정본

--- lib/screens/search_page.dart
+++ lib/screens/search_page.dart
@@ -1,6 +1,7 @@
 //검색 페이지 ( search_page.dart )
 
 import 'package:flutter/material.dart';
+import 'package:insta2/scripts.dart';
 import 'package:insta2/widgets/navigatorList.dart';
 
 class search_page extends StatefulWidget {
@@ -54,68 +55,89 @@ class _searchState extends State<search_page> {
     return Scaffold(
       body: Row(
         children: [
-          navigatorList(),
-          Padding(
-            padding: const EdgeInsets.only(top: 30),
-            child: Container(
-              width: MediaQuery.of(context).size.width - 260,
-              child: Column(children: [
-                Image.asset('images/instagram_logo_nemo.png', height: 300),
+          Visibility(
+            visible: checkNumBiggerWidth(243, context),
+            child: navigatorList(),
+          ),
+          Visibility(
+            visible: checkNumBiggerWidth(243 + 800, context),
+            child: Expanded(
+              child: Container(
+                height: MediaQuery.of(context).size.height,
+                child: SingleChildScrollView(
+                  child: Padding(
+                    padding: const EdgeInsets.only(top: 30),
+                    child: Container(
+                      width: MediaQuery.of(context).size.width - 260,
+                      child: Column(children: [
+                        Image.asset('images/instagram_logo_nemo.png',
+                            height: 300),
 
-                //검색창
-                Container(
-                  width: 800,
-                  height: 50,
-                  child: TextField(
-                    cursorColor: Colors.black,
-                    focusNode: focusNode,
-                    style: TextStyle(
-                      color: Colors.black54,
-                      fontSize: 15,
-                    ),
-                    autofocus: true,
-                    controller: _filter,
-                    decoration: InputDecoration(
-                      labelText: '검색',
-                      labelStyle: TextStyle(color: Colors.black),
-                      enabledBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.all(Radius.circular(3)),
-                        borderSide: BorderSide(color: Colors.grey),
-                      ),
-                      focusedBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.all(Radius.circular(3)),
-                        borderSide: BorderSide(color: Colors.grey),
-                      ),
-
-                      //검색 아이콘
-                      prefixIcon: Icon(
-                        Icons.search,
-                        color: Colors.black54,
-                        size: 20,
-                      ),
-
-                      //검색에서 x 누르면 클리어
-                      suffixIcon: focusNode.hasFocus
-                          ? IconButton(
-                              icon: Icon(
-                                Icons.cancel,
-                                size: 20,
-                                color: Colors.grey,
+                        //검색창
+                        Container(
+                          width: 800,
+                          height: 50,
+                          child: TextField(
+                            cursorColor: Colors.black,
+                            focusNode: focusNode,
+                            style: TextStyle(
+                              color: Colors.black54,
+                              fontSize: 15,
+                            ),
+                            autofocus: true,
+                            controller: _filter,
+                            decoration: InputDecoration(
+                              labelText: '검색',
+                              labelStyle: TextStyle(color: Colors.black),
+                              enabledBorder: OutlineInputBorder(
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(3)),
+                                borderSide: BorderSide(color: Colors.grey),
                               ),
-                              onPressed: () {
-                                setState(() {
-                                  _filter.clear();
-                                  focusNode.unfocus();
-                                  _searchText = "";
-                                });
-                              },
-                            )
-                          : Container(),
+                              focusedBorder: OutlineInputBorder(
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(3)),
+                                borderSide: BorderSide(color: Colors.grey),
+                              ),
+
+                              //검색 아이콘
+                              prefixIcon: Icon(
+                                Icons.search,
+                                color: Colors.black54,
+                                size: 20,
+                              ),
+
+                              //검색에서 x 누르면 클리어
+                              suffixIcon: focusNode.hasFocus
+                                  ? IconButton(
+                                      icon: Icon(
+                                        Icons.cancel,
+                                        size: 20,
+                                        color: Colors.grey,
+                                      ),
+                                      onPressed: () {
+                                        setState(() {
+                                          _filter.clear();
+                                          focusNode.unfocus();
+                                          _searchText = "";
+                                        });
+                                      },
+                                    )
+                                  : Container(),
+                            ),
+                          ),
+                        ),
+                        if (_filter.text != '') ...[
+                          _search_history()
+                        ] else ...[
+                          Container()
+                        ],
+                        //focusNode.hasFocus ? _search_history() : Container()
+                      ]),
                     ),
                   ),
                 ),
-                focusNode.hasFocus ? _search_history() : Container()
-              ]),
+              ),
             ),
           ),
         ],
