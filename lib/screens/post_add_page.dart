7d412945d2cdf44b26486137e9ef8d2786b98813
Hyeon-A-Lee <gusdk0201@naver.com>
2022-12-01 16:40:40 +0900

    border underline 색 변경

--- lib/screens/post_add_page.dart
+++ lib/screens/post_add_page.dart
@@ -72,9 +72,12 @@ class _MyWidgetState extends State<PostAddPage> {
             cursorColor: Colors.black,
             controller: contents,
             decoration: InputDecoration(
-              labelText: '내용을 입력하세요.',
-              labelStyle: TextStyle(color: Colors.black),
-            ),
+                labelText: '내용을 입력하세요.',
+                labelStyle: TextStyle(color: Colors.black),
+                enabledBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(color: Colors.black26)),
+                focusedBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(color: Colors.black26))),
           ),
         ),
       ],
