6d160feb5ed70a3b247b1b9fd77a6da88b7bef59
Hyeon-A-Lee <gusdk0201@naver.com>
2022-12-02 11:37:57 +0900

    댓글 10개 뜨게 수정

--- lib/screens/comment.dart
+++ lib/screens/comment.dart
@@ -16,6 +16,7 @@ TextEditingController mycomment = TextEditingController();
 
 class _CommentState extends State<Comment> {
   int h_color = 0, h_count = 0;
+
   @override
   Widget build(BuildContext context) {
     providerVariable provar = Provider.of<providerVariable>(context);
@@ -104,95 +105,114 @@ class _CommentState extends State<Comment> {
                 color: Colors.grey,
               ),
               */
-                        //댓글
                         Column(
                           children: [
-                            Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                              children: [
-                                Container(
-                                  child: Row(
+                            for (int i = 0; i < 10; i++) ...[
+                              Container(
+                                height: 3,
+                                width: 800,
+                                color: Colors.white,
+                              ),
+                              Column(
+                                children: [
+                                  Row(
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceBetween,
                                     children: [
-                                      //댓글 프로필 이미지를 Stack으로 구현
-                                      Stack(
-                                        children: [
-                                          if (provar.checkmyimage == true)
-                                            Image.file(
-                                              provar.myimage,
-                                              width: 40,
-                                              height: 40,
+                                      Container(
+                                        child: Row(
+                                          children: [
+                                            //댓글 프로필 이미지를 Stack으로 구현
+                                            Stack(
+                                              children: [
+                                                if (provar.checkmyimage == true)
+                                                  Image.file(
+                                                    provar.myimage,
+                                                    width: 40,
+                                                    height: 40,
+                                                  ),
+                                                if (provar.checkmyimage ==
+                                                    false)
+                                                  Image.asset(
+                                                    'images/normal_profile.png',
+                                                    width: 40,
+                                                    height: 40,
+                                                  ),
+                                                Image.asset(
+                                                  'images/frame.png',
+                                                  width: 40,
+                                                  height: 40,
+                                                ),
+                                              ],
                                             ),
-                                          if (provar.checkmyimage == false)
-                                            Image.asset(
-                                              'images/normal_profile.png',
-                                              width: 40,
-                                              height: 40,
+                                            //댓글 프로필 아이디와 실제 댓글 구현/중간에 padding추가
+                                            Text(
+                                              "ID",
+                                              style: TextStyle(
+                                                  fontSize: 25,
+                                                  color: Colors.black),
                                             ),
-                                          Image.asset(
-                                            'images/frame.png',
-                                            width: 40,
-                                            height: 40,
-                                          ),
-                                        ],
+                                            Padding(
+                                                padding: EdgeInsets.all(15)),
+                                            Text(
+                                              "comment",
+                                              style: TextStyle(
+                                                  fontSize: 25,
+                                                  color: Colors.black),
+                                            ),
+                                          ],
+                                        ),
                                       ),
-                                      //댓글 프로필 아이디와 실제 댓글 구현/중간에 padding추가
-                                      Text(
-                                        "ID",
-                                        style: TextStyle(
-                                            fontSize: 25, color: Colors.black),
-                                      ),
-                                      Padding(padding: EdgeInsets.all(15)),
-                                      Text(
-                                        "comment",
-                                        style: TextStyle(
-                                            fontSize: 25, color: Colors.black),
-                                      ),
-                                    ],
-                                  ),
-                                ),
-                                Container(
-                                  child: Row(
-                                    children: [
-                                      //댓글 좋아요 갯수 Text로 구현
-                                      Text(
-                                        "좋아요: " + h_count.toString() + "개",
-                                        style: TextStyle(
-                                            fontSize: 15, color: Colors.grey),
-                                      ),
-                                      //♡(빈 하트) IconButton 구현 시도, onPress() 시 변환 아직 불가
-                                      IconButton(
-                                        icon: Icon(Icons.favorite_border),
-                                        onPressed: () {
-                                          h_color += 1;
-                                          if (h_color % 2 == 1) {
-                                            onTap:
-                                            () {
-                                              icon:
-                                              Icon(Icons.favorite_outlined,
-                                                  color: Colors.red);
-                                            };
-                                            setState(() {
-                                              h_count += 1;
-                                            });
-                                          } else {
-                                            onTap:
-                                            () {
-                                              icon:
-                                              Icon(Icons.favorite_border);
-                                            };
-                                            setState(
-                                              () {
-                                                h_count -= 1;
+                                      Container(
+                                        child: Row(
+                                          children: [
+                                            //댓글 좋아요 갯수 Text로 구현
+                                            Text(
+                                              "좋아요: " +
+                                                  h_count.toString() +
+                                                  "개",
+                                              style: TextStyle(
+                                                  fontSize: 15,
+                                                  color: Colors.grey),
+                                            ),
+                                            //♡(빈 하트) IconButton 구현 시도, onPress() 시 변환 아직 불가
+                                            IconButton(
+                                              icon: Icon(Icons.favorite_border),
+                                              onPressed: () {
+                                                h_color += 1;
+                                                if (h_color % 2 == 1) {
+                                                  onTap:
+                                                  () {
+                                                    icon:
+                                                    Icon(
+                                                        Icons.favorite_outlined,
+                                                        color: Colors.red);
+                                                  };
+                                                  setState(() {
+                                                    h_count += 1;
+                                                  });
+                                                } else {
+                                                  onTap:
+                                                  () {
+                                                    icon:
+                                                    Icon(Icons.favorite_border);
+                                                  };
+                                                  setState(
+                                                    () {
+                                                      h_count -= 1;
+                                                    },
+                                                  );
+                                                }
                                               },
-                                            );
-                                          }
-                                        },
+                                            ),
+                                          ],
+                                        ),
                                       ),
                                     ],
                                   ),
-                                ),
-                              ],
-                            ),
+                                ],
+                              )
+                            ],
                           ],
                         ),
                       ],
