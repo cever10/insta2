1e11901f0604610ef373b788febee766cd7c3301
cever10 <jiwan_01@naver.com>
2022-12-13 13:55:12 +0900

    숫자 떠있는게 싫어서 한거

--- lib/screens/anotheruser_page.dart
+++ lib/screens/anotheruser_page.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 import 'package:insta2/providerVar/providerVars.dart';
 import 'package:insta2/scripts.dart';
 import 'package:insta2/widgets/floatingInstaFeed.dart';
